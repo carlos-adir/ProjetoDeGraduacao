e5de8b607fc5d970e73675a8475e75e21c25cb52
Carlos Adir <carlos.adir.leite@gmail.com>
2022-09-10 15:01:45 -0300

    Update file to read data from results

--- Poisson/read_data_from_files.py
+++ Poisson/read_data_from_files.py
@@ -1,7 +1,8 @@
 import re
+import os
 from os import listdir
 from os.path import isfile, join
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 def get_files(folder: str) -> List[str]:
     onlyfiles = [f for f in listdir(folder) if isfile(join(folder, f))]
@@ -18,7 +19,7 @@ def fileexists(filename: str) -> bool:
 def readfile(filename: str) -> str:
     if not fileexists:
         raise ValueError(f"Tried to read {filename}, but doesn't exist")
-    with open(filename) as file:
+    with open(filename, "r") as file:
         lines = file.readlines()
     return '\n'.join(lines)
 
@@ -28,6 +29,25 @@ def filter_files(listfilesnames: List[str], key: str):
         if key in filename:
             newlistfilesnames.append(filename)
     return newlistfilesnames
+
+def get_logview_table(filename: str) -> Dict:
+    texto = readfile(filename)
+    regexlinha = r"\[0\] ([a-zA-Z]+) \s+([0-9\s.e+-]+)\[0\] ?\n"
+    encontros = re.findall(regexlinha, texto)
+    for i, e in enumerate(encontros):
+        nome = e[0]
+        numeros = e[1].split(" ")
+        while '' in numeros:
+            numeros.remove('')
+        for j, n in enumerate(numeros):
+            try:
+                numeros[j] = float(n)
+            except ValueError:
+                indexe = n.index("e")
+                numeros[j] = float(n[:indexe+3])
+        print("nome = ", nome)
+        # print("numeros = ", numeros)
+        print("len(numeros) = ", len(numeros))
 
 def get_pnt(filename: str) -> Tuple[int, int, float]:
     """
@@ -73,3 +93,22 @@ def getdata_pnt(folder: str, key: str) -> List[Tuple[int, int, float]]:
         except Exception as e:
             print(f"For the file '{filename}', there was an error. Could not plot this value.")
     return results
+
+
+
+
+def main():
+    """
+    Para testar as funcoes
+    """
+    currentfolder = os.getcwd()
+    currentfolder = currentfolder.replace("\\", "/")
+    foldername = "Poisson/results-e2highcpu8-13-08-2022"
+    filename = "poisson2D_ksp-p6-mesh129x129.txt"
+    completefilename = currentfolder + "/" + foldername + "/" + filename
+    get_logview_table(completefilename)
+
+
+
+if __name__ == "__main__":
+    main()
